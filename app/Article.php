a9f9be1b88f360cf9871277272c51ddc348e3698
Meido <popenkodaniil@mail.ru>
2020-03-25 14:43:54 +0700

    Добавлена функция its_author

--- app/Article.php
+++ app/Article.php
@@ -13,6 +13,18 @@ class Article extends Model
 
     protected $dates = ['deleted_at'];
 
+    public function its_author($user){
+        if(get_class($user) === 'App\User'){
+            if($this->trashed()) return NULL;
+            foreach ($this->users() as $author) {
+                if($author == $user) return TRUE;
+            }
+            return FALSE;
+        }else{
+            throw new InvalidArgumentException('Its not User!');
+        };
+    }
+
     public function users()
     {
         return $this->belongsToMany('App\User', 'article_user', 'article_id', 'user_id');
